8e77f0b2a218c3334f98b7a5ae2730ec19c308d1
Michael Castillo <lsxkiller9000@gmail.com>
2018-07-16 16:22:05 -0400

    working on homework 1
    Still need to finish up part 3 but part 1 and 2 are finished

---
+++ homework1.py
@@ -0,0 +1,28 @@
+#homework 1
+
+#part 1
+print("Homework 1, part 1")
+print()
+my_name = "Michael"
+age = 23
+x = age + 102
+
+print("Hello, " + my_name + ", in 102 years you will be " + str(x))
+print()
+
+#part 2
+print("Homework 1, part 2")
+print()
+print(" | | ")
+print("-+-+-")
+print("-+-+-")
+print(" | | ")
+print()
+
+#part 3
+print("Homework 1, part 3")
+print()
+
+a = input("Enter a Number for A")
+b = input("Enter a Number for B")
+c = input("Enter a Number for C")
