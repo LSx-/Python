bcca9d6e4176c954b78acbde8de83a32dda62a30
Michael Castillo <7505231+LSx-@users.noreply.github.com>
2018-07-17 01:50:56 -0400

    homework1-2
    Homework 1 is finished! didnt start homework 2

--- homework1.py
+++ homework1.py
@@ -22,7 +22,15 @@ print()
 #part 3
 print("Homework 1, part 3")
 print()
+print("The standard form of a line is 0=Ay+Bx+C, where A,B, and C are real numbers")
+a = input("Enter a Number for A: ")
+b = input("Enter a Number for B: ")
+c = input("Enter a Number for C: ")
 
-a = input("Enter a Number for A")
-b = input("Enter a Number for B")
-c = input("Enter a Number for C")
+#now we will do the x and y interpets
+
+x_int = -int(c)/int(b)
+y_int = -int(c)/int(a)
+
+print("The x-intercept of your line is: " + str(x_int))
+print(" and your y intercept is: " + str(y_int))
